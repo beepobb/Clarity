dcb6c2087cf71196213b752ae61681747d333397
loheesong <loheesong@gmail.com>
2024-03-20 13:57:38 +0800

    feat: nav bar with 5 fragments

--- app/src/main/java/com/example/clarity/MainActivity.java
+++ app/src/main/java/com/example/clarity/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.clarity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.clarity.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Discover());
+
+        // set click listeners to nav bar
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemID = item.getItemId();
+
+            if (itemID == R.id.Discover) {
+                replaceFragment(new Discover());
+            } else if (itemID == R.id.Favourites) {
+                replaceFragment(new Favourites());
+            } else if (itemID == R.id.Create) {
+                replaceFragment(new Create());
+            } else if (itemID == R.id.Calendar) {
+                replaceFragment(new Calendar());
+            } else if (itemID == R.id.Profile) {
+                replaceFragment(new Profile());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
+
