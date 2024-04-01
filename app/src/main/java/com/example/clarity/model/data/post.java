7a116ac7147e47f6f99ae79171089d98e8207bec
loheesong <loheesong@gmail.com>
2024-04-01 19:26:04 +0800

    Added Post class

---
+++ app/src/main/java/com/example/clarity/model/data/post.java
@@ -0,0 +1,13 @@
+package com.example.clarity.model.data;
+
+public class post {
+    private int id;
+    private int author_id;
+    private String event_start;
+    private String event_end;
+    private String image_url;
+    private String title;
+    private String location;
+    private String description;
+    private String created_at;
+}
