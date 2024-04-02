29c0d2278ed3150ed54a4a4fcc9a39218cfbbe7b
Cai Junjie <junjie.50@hotmail.com>
2024-04-02 14:05:37 +0800

    bug fix in mainActivity

--- app/src/main/java/com/example/clarity/MainActivity.java
+++ app/src/main/java/com/example/clarity/MainActivity.java
@@ -30,11 +30,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-<<<<<<< HEAD
         replaceFragment(new CalendarFragment());
-=======
         replaceFragment(new Discover());
->>>>>>> b149a27c533ebcad2b0d412690d844279e6fc663
 
         // set click listeners to nav bar
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
