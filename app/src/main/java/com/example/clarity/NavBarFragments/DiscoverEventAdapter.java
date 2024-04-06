478f8449dbb04c51bd6bf4b4fd144d35c1b1a96d
loheesong <loheesong@gmail.com>
2024-04-06 19:02:32 +0800

    refactor: discover fragment

---
+++ app/src/main/java/com/example/clarity/NavBarFragments/DiscoverEventAdapter.java
@@ -0,0 +1,69 @@
+package com.example.clarity.NavBarFragments;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clarity.R;
+import com.example.clarity.model.data.Post;
+
+import java.util.List;
+
+public class DiscoverEventAdapter extends  RecyclerView.Adapter<DiscoverEventAdapter.DiscoverEventViewHolder> {
+    private Context context;
+    private List<Post> eventList;
+
+    public DiscoverEventAdapter(Context context, List<Post> eventList) {
+        this.context = context;
+        this.eventList = eventList;
+    }
+    public static class DiscoverEventViewHolder extends RecyclerView.ViewHolder {
+        ImageView eventImage;
+        TextView eventName;
+        TextView eventDate;
+        TextView eventTime;
+        TextView eventLocation;
+
+        public DiscoverEventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            eventImage = itemView.findViewById((R.id.eventImage));
+            eventName = itemView.findViewById((R.id.eventName));
+            eventDate = itemView.findViewById((R.id.eventDate));
+            eventTime = itemView.findViewById((R.id.eventTime));
+            eventLocation = itemView.findViewById((R.id.eventLocation));
+        }
+    }
+
+    @NonNull
+    @Override
+    public DiscoverEventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("DiscoverEventAdapter", "onCreateViewHolder");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_item_model, parent, false);
+        return new DiscoverEventViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DiscoverEventViewHolder holder, int position) {
+        Log.d("DiscoverEventAdapter", "onBindViewHolder");
+        Post event_details = eventList.get(position);
+
+        // bind all content to UI
+//        holder.eventImage.setImageResource(1);
+        holder.eventName.setText(event_details.getTitle());
+        holder.eventDate.setText(event_details.getEvent_start());
+        holder.eventTime.setText(event_details.getEvent_start());
+        holder.eventLocation.setText(event_details.getLocation());
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventList.size();
+    }
+}
---
+++ app/src/main/java/com/example/clarity/NavBarFragments/TagButtonAdapter.java
@@ -0,0 +1,68 @@
+package com.example.clarity.NavBarFragments;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clarity.R;
+
+import java.util.List;
+
+public class TagButtonAdapter extends RecyclerView.Adapter<TagButtonAdapter.TagButtonViewHolder>{
+
+    private Context context;
+    private List<Discover.EventTags> buttonNameList;
+    private List<Button> buttonList;
+    public TagButtonAdapter(Context context, List<Discover.EventTags> buttonNameList) {
+        this.context = context;
+        this.buttonNameList = buttonNameList;
+        Log.d("TagButtonAdapter", "TagButtonAdapter");
+    }
+
+    public static class TagButtonViewHolder extends RecyclerView.ViewHolder {
+        Button button;
+        public TagButtonViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Log.d("TagButtonAdapter", "TagButtonViewHolder");
+            button = itemView.findViewById((R.id.tag_button_model));
+        }
+    }
+
+    @NonNull
+    @Override
+    public TagButtonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("TagButtonAdapter", "onCreateViewHolder");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.tag_button_model, parent, false);
+        return new TagButtonViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TagButtonViewHolder holder, int position) {
+        // RecyclerView calls this to bind a EventViewHolder to data
+        Log.d("TagButtonAdapter", "onBindViewHolder");
+        Discover.EventTags buttonName = buttonNameList.get(position);
+
+        // bind all content to UI
+        holder.button.setText(buttonName.toString());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // To be set
+                Toast.makeText(context, holder.button.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return buttonNameList.size();
+    }
+}
