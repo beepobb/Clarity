d0f70cb3746f526b4efbe265ce2edcf341126feb
pankti0 <89905861+pankti0@users.noreply.github.com>
2024-04-07 13:19:41 +0800

    Changed events to subactivity from fragment

---
+++ app/src/main/java/com/example/clarity/Events.java
@@ -0,0 +1,17 @@
+package com.example.clarity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Events extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.events, container, false);
+        return rootView;
+    }
+}
