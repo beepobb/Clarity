ac53051319a86bd85729a43f6fc9bbb452d68cd9
pankti0 <89905861+pankti0@users.noreply.github.com>
2024-04-07 17:11:00 +0800

    events page completed

--- app/src/main/java/com/example/clarity/Events.java
+++ app/src/main/java/com/example/clarity/Events.java
@@ -1,17 +1,14 @@
 package com.example.clarity;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Events extends Fragment {
+
+public class Events extends AppCompatActivity {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.events, container, false);
-        return rootView;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events);
     }
 }
