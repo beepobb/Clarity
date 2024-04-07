1f39d23470120ab97d81d00022fd8ad4c19ad163
gontinus <sunsitongsammy@163.com>
2024-04-07 17:14:56 +0800

    Implement create page upload image

--- app/src/main/java/com/example/clarity/NavBarFragments/Create.java
+++ app/src/main/java/com/example/clarity/NavBarFragments/Create.java
@@ -1,66 +1,57 @@
 package com.example.clarity.NavBarFragments;
 
-import android.os.Bundle;
+import static android.app.Activity.RESULT_OK;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.net.Uri;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.clarity.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Create#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Create extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Create() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Create.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Create newInstance(String param1, String param2) {
-        Create fragment = new Create();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private ActivityResultLauncher<Intent> imageActivityResultLauncher;
+    private ImageView selectedImageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_create, container, false);
+        selectedImageView = rootview.findViewById(R.id.placeHolder);
+        imageActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            Uri selectedImageUri = data.getData();
+                            // Load the selected image into the ImageView
+                            selectedImageView.setImageURI(selectedImageUri);
+                        }
+                    }
+                });
+
+        // Launch the gallery picker when the ImageView is clicked
+        selectedImageView.setOnClickListener(view -> selectImage());
+        return rootview;
+    }
+
+    public void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        if (intent.resolveActivity(requireContext().getPackageManager()) != null) {
+            imageActivityResultLauncher.launch(intent);
+        }
     }
 }
