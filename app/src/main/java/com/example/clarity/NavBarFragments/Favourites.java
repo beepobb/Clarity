c7061c48a76ec9c6ca0c603e1a867f804619339b
beepobb <yeejiazhen23@gmail.com>
2024-04-09 14:50:28 +0800

    feat: added edit profile, change password and log out

--- app/src/main/java/com/example/clarity/NavBarFragments/Favourites.java
+++ app/src/main/java/com/example/clarity/NavBarFragments/Favourites.java
@@ -1,66 +1,55 @@
 package com.example.clarity.NavBarFragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.clarity.MainActivity;
 import com.example.clarity.R;
+import com.example.clarity.model.data.Post;
+import com.example.clarity.model.data.User;
+import com.example.clarity.model.repository.RestRepo;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Favourites#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class Favourites extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Favourites() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Favourites.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Favourites newInstance(String param1, String param2) {
-        Favourites fragment = new Favourites();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private RestRepo db;
+    private User user;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourites, container, false);
+        View view = inflater.inflate(R.layout.fragment_favourites, container, false);
+        Activity activity = getActivity();
+        if (activity != null) {
+            // Example: Accessing activity's method
+            db = ((MainActivity) activity).database;
+        }
+        user = new User(3, "ifalltower", "jeui3ug4i836", "SUTD Student", "test@gmail.com", "2024-04-01 06:35:23");
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        db.getFavouritesRequest(user.getId(), new RestRepo.RepositoryCallback<ArrayList<Post>>() {
+            @Override
+            public void onComplete(ArrayList<Post> result) {
+
+            }
+        });
     }
 }
