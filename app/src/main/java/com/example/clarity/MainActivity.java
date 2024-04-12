f16f627ddd21f0b4a2d16a38cd66dd52cd84c23e
Cai Junjie <junjie.50@hotmail.com>
2024-04-12 20:36:08 +0800

    Feat: Added support for adding profile images to username.

--- app/src/main/java/com/example/clarity/MainActivity.java
+++ app/src/main/java/com/example/clarity/MainActivity.java
@@ -51,7 +51,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         discoverFragment = new Discover();
         favouritesFragment = new Favourites();
         createFragment = new Create();
