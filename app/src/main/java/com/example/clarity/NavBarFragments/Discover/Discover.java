c537aab69e2eac50e79f2be36896d636caee73b2
beepobb <yeejiazhen23@gmail.com>
2024-04-14 19:31:52 +0800

    feat: pre-select first category 'All' on startup

--- app/src/main/java/com/example/clarity/NavBarFragments/Discover/Discover.java
+++ app/src/main/java/com/example/clarity/NavBarFragments/Discover/Discover.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -85,6 +86,7 @@ public class Discover extends Fragment implements TagButtonUpdateEventsClickList
         tagRecycler.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
         tagButtonAdapter = new TagButtonAdapter(getActivity(), tagButtons, this);
         tagRecycler.setAdapter(tagButtonAdapter);
+
 
         // set up event recycler
         eventRecycler.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
