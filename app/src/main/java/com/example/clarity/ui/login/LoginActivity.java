552f1f93eb1232ebd1e7df004755187673019cdc
hoxiaoyang <xiaoyang_ho@mymail.sutd.edu.sg>
2024-04-19 13:31:23 +0800

    updated comments

--- app/src/main/java/com/example/clarity/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/clarity/ui/login/LoginActivity.java
@@ -37,13 +37,7 @@ public class LoginActivity extends AppCompatActivity {
     private Handler handler;
     private ProgressBar progressBar;
 
-    /**
-     *
-     * @param savedInstanceState If the activity is being re-initialized after
-     *     previously being shut down then this Bundle contains the data it most
-     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
-     *
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
